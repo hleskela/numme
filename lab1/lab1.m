fd9414224c2fb030e2eaae6aff5054b9d92a68c0
Hannes Leskelä <hleskela@kth.se>
2016-04-22 16:07:30 +0200

    Add entire part 3 in code (4 and 5 left)

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -48,7 +48,7 @@ previous_guess = 0;
 iterations = 0;
 while abs(guess - previous_guess) > 10^-10
     iterations = iterations +1;
-    previous_guess = guess;
+    previous_guess = guess
     guess = guess - (f(guess)/df(guess));
 end
 disp('Done with Newton-Raphsons method')
@@ -56,3 +56,23 @@ guess
 iterations
 % root = -0.8984 in 3 iterations
 % root = 4.5178 in 2 iterations
+%  0.017789514180033
+% 1.225604920449541e-04
+% -5.424547033783256e-09
+% Using above with pn = log(en+1)-log(en) / log(en) - log(en-1)
+% http://www.wolframalpha.com/input/?i=%28log%285.424547033783256*10^-9%29-log%281.225604920449541*10^-4%29%29%2F%28log%281.225604920449541*10^-4%29+-+log%280.017789514180033%29%29
+
+%sekantmetoden
+xx = 4;
+yy= 5;
+previous = 0;
+iterations = 0;
+while abs(xx - yy) > 10^-10
+    iterations = iterations + 1
+    disp('Hannes')
+    tmp = yy
+    yy = yy - (((yy - xx)/(f(yy) - f(xx)) * f(yy)));
+    xx = tmp
+end
+yy
+iterations
